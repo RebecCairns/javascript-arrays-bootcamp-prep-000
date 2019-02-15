e40b5b2225e253e4ef9a90badb10a741db8331c4
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-15 19:54:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+function addElementToBeginningOfArray(array101, newVal){
+  var array202 = [newVal, ...array101]
+  return array202;
+}
+function destructivelyAddElementToBeginningOfArray(array101, newVal){
+   array101.unshift(newVal);
+   return array101; 
+}
+function addElementToEndOfArray(array303, newVal3){
+  var arrayLength = array303.length;
+  //array303[arrayLength] = arrayLength;
+  array303[arrayLength] = newVal3;
+  return array303;
+}
+function destructivelyAddElementToEndOfArray(array404, newVal4){
+  array404.push(newVal4);
+  return array404; 
+}
+function removeElementFromBeginningOfArra(array101){
+  var newArray = array101.slice(1);
+  return newArray;
+}
+function destructivelyRemoveElementFromEndOfArray(array101){
+   var newArray2 =  array101.pop()
+   return newArray2;
+}
+function removeElementFromEndOfArray(array101){
+  var newArray3 = array101.slice(0, array101.length() - 1);
+  return newArray3;
+}
